b1527ee387684bcaa99a39fbc405ee31010e445f
sliva0 <sliva0mk@gmail.com>
2022-05-27 01:46:42 +0100

    create ObjectPolygon constructor

--- src/types/objects/polygons.rs
+++ src/types/objects/polygons.rs
@@ -74,16 +74,15 @@ impl Polygon {
         let dist = self.plane.find_intersection(start, dir)?;
         let pos = start + dir * dist;
 
-        let m = get_pairs(
-            [
-                self.e[0] ^ (self.v[0] >> pos),
-                self.e[1] ^ (self.v[1] >> pos),
-                self.e[2] ^ (self.v[2] >> pos),
-            ],
+        let m = get_vec_pairs(
+            self.e
+                .into_iter()
+                .zip(self.v)
+                .map(|(ei, vi)| ei ^ (vi >> pos))
+                .collect(),
             |v1, v2| v1 * v2,
         );
-        m.iter().all(|&x| x >= 0.0);
-        if m[0].min(m[1]).min(m[2]) >= 0.0 {
+        if m.into_iter().all(|x| x >= 0.0) {
             Some(dist)
         } else {
             None
@@ -99,6 +98,14 @@ pub struct ObjectPolygon<T: MetaTracingObject> {
     p: Polygon,
     obj: Weak<T>,
 }
+impl<T: MetaTracingObject> ObjectPolygon<T> {
+    fn new(v: [Point; 3], obj: &Weak<T>) -> Arc<Self> {
+        Arc::new(Self {
+            p: Polygon::new(v),
+            obj: obj.clone(),
+        })
+    }
+}
 impl<'a, T: MetaTracingObject + 'a + Sync + Send> ObjectPolygon<T> {
     pub fn collect_cuboid_face(
         obj: Weak<T>,
@@ -107,8 +114,9 @@ impl<'a, T: MetaTracingObject + 'a + Sync + Send> ObjectPolygon<T> {
         sides: (Vector, Vector),
     ) -> Vec<TracingObjectType<'a>> {
         let center = shift + dir;
-        let eps1: f64 = 1.0 + EPSILON;
-        let sides = (sides.0 * eps1, sides.1 * eps1);
+        // I do not remember why these lines are needed, if I remember, I will return it
+        // let eps1: f64 = 1.0 + EPSILON;
+        // let sides = (sides.0 * eps1, sides.1 * eps1);
         let c = (
             center + sides.0 + sides.1,
             center - sides.0 + sides.1,
@@ -116,14 +124,8 @@ impl<'a, T: MetaTracingObject + 'a + Sync + Send> ObjectPolygon<T> {
             center + sides.0 - sides.1,
         );
         vec![
-            Arc::new(Self {
-                p: Polygon::new([c.0, c.1, c.2]),
-                obj: obj.clone(),
-            }),
-            Arc::new(Self {
-                p: Polygon::new([c.2, c.3, c.0]),
-                obj: obj.clone(),
-            }),
+            Self::new([c.0, c.1, c.2], &obj),
+            Self::new([c.2, c.3, c.0], &obj),
         ]
     }
 }
