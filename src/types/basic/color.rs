5f3aab7964cc08ca458b74989ba2817be44f2f95
sliva0 <sliva0mk@gmail.com>
2022-07-04 00:36:28 +0100

    rewrite Color realization to iter_fixed

--- src/types/basic/color.rs
+++ src/types/basic/color.rs
@@ -1,70 +1,57 @@
 use std::{
+    array::IntoIter,
     iter::Sum,
     ops::{Add, AddAssign, Div, Mul, MulAssign},
 };
 
 use image::Rgb;
+use iter_fixed::{IntoIteratorFixed, IteratorFixed};
 
 pub type RawColor = Rgb<u8>;
 
 #[derive(Debug, Copy, Clone)]
-pub struct Color {
-    r: f64,
-    g: f64,
-    b: f64,
-}
+pub struct Color([f64; 3]);
+
 impl Color {
-    pub const BLACK: Color = Color {
-        r: 0.0,
-        g: 0.0,
-        b: 0.0,
-    };
-    pub const ERR_COLOR: Color = Color {
-        r: 0.0,
-        g: 255.0,
-        b: 0.0,
-    };
+    pub const BLACK: Color = Color([0.0, 0.0, 0.0]);
+    pub const ERR_COLOR: Color = Color([0.0, 1.0, 0.0]);
 
-    fn cut_f64(n: f64) -> f64 {
-        const MIN: f64 = 0.0;
-        const MAX: f64 = 255.0;
-
-        match n {
-            n if n > MAX => MAX,
-            n if n < MIN => MIN,
-            n => n,
-        }
+    fn cut_value(n: f64) -> f64 {
+        n.clamp(0.0, 1.0)
     }
+
+    fn convert_value(v: i32) -> f64 {
+        Self::cut_value(v as f64 / 255.0)
+    }
+
     fn cut(&self) -> Self {
-        Self {
-            r: Self::cut_f64(self.r),
-            g: Self::cut_f64(self.g),
-            b: Self::cut_f64(self.b),
-        }
+        self.fixed_iter().map(Self::cut_value).into()
     }
+
     pub fn new(r: i32, g: i32, b: i32) -> Self {
-        Self {
-            r: r as f64,
-            g: g as f64,
-            b: b as f64,
-        }
-        .cut()
+        [r, g, b].into_iter_fixed().map(Self::convert_value).into()
     }
+
     pub fn raw(self) -> RawColor {
         self.into()
     }
 
+    fn fixed_iter(&self) -> IteratorFixed<IntoIter<f64, 3>, 3> {
+        self.0.into_iter_fixed()
+    }
+
     fn diff(&self, rhs: &Self) -> f64 {
-        let sub = [
-            (self.r - rhs.r).abs(),
-            (self.g - rhs.g).abs(),
-            (self.b - rhs.b).abs(),
-        ];
-        sub.iter().sum::<f64>() / (255.0 * 3.0)
+        self.0
+            .into_iter()
+            .zip(rhs.0)
+            .map(|(s, r)| (s - r).abs())
+            .sum::<f64>()
+            / 3.0
     }
 
     pub fn colors_diff(colors: &Vec<Self>) -> f64 {
         let mut max_diff = 0f64;
+
         for i in colors.iter() {
             for j in colors.iter() {
                 max_diff = max_diff.max(i.diff(j));
@@ -75,18 +62,15 @@ impl Color {
 
     pub fn colors_avg(colors: Vec<Self>) -> Self {
         let len = colors.len();
-        colors.into_iter().sum::<Color>() / len as f64
+        let sum: Color = colors.into_iter().sum();
+        sum / (len as f64)
     }
 }
 
 impl Add for Color {
     type Output = Self;
     fn add(self, rhs: Self) -> Self {
-        Self {
-            r: self.r + rhs.r,
-            g: self.g + rhs.g,
-            b: self.b + rhs.b,
-        }
+        self.fixed_iter().zip(rhs.0).map(|(a, b)| a + b).into()
     }
 }
 
@@ -99,11 +83,7 @@ impl AddAssign<Color> for Color {
 impl Mul<f64> for Color {
     type Output = Self;
     fn mul(self, rhs: f64) -> Self {
-        Self {
-            r: self.r * rhs,
-            g: self.g * rhs,
-            b: self.b * rhs,
-        }
+        self.fixed_iter().map(|a| a * rhs).into()
     }
 }
 
@@ -116,34 +96,41 @@ impl MulAssign<f64> for Color {
 impl Div<f64> for Color {
     type Output = Self;
     fn div(self, rhs: f64) -> Self {
-        Self {
-            r: self.r / rhs,
-            g: self.g / rhs,
-            b: self.b / rhs,
-        }
+        self.fixed_iter().map(|a| a / rhs).into()
     }
 }
 
 impl Mul for Color {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self {
-        Self {
-            r: self.r * rhs.r / 255.0,
-            g: self.g * rhs.g / 255.0,
-            b: self.b * rhs.b / 255.0,
-        }
+        self.fixed_iter().zip(rhs.0).map(|(a, b)| a * b).into()
     }
 }
 
 impl Sum for Color {
     fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
-        iter.fold(Color::BLACK, |a, b| a + b)
+        iter.fold(Color::BLACK, Color::add)
     }
 }
 
 impl From<Color> for RawColor {
     fn from(color: Color) -> Self {
-        let color = color.cut();
-        Rgb([color.r as u8, color.g as u8, color.b as u8])
+        Rgb(color
+            .cut()
+            .fixed_iter()
+            .map(|x| (x * 255.0) as u8)
+            .collect())
+    }
+}
+
+impl From<[f64; 3]> for Color {
+    fn from(v: [f64; 3]) -> Self {
+        Self(v)
+    }
+}
+
+impl<I: Iterator<Item = f64>> From<IteratorFixed<I, 3>> for Color {
+    fn from(iter: IteratorFixed<I, 3>) -> Self {
+        iter.collect::<[f64; 3]>().into()
     }
 }
